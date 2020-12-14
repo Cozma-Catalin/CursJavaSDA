e4df7140c9186d61f9a0bec617464e1a72b095be
Cozma Catalin <cozmacatalindaniel@gmail.com>
2020-12-15 00:01:22 +0200

    improved password crypting

--- CursJavaPractic/FinalProject/src/main/java/business/service/AccountService.java
+++ CursJavaPractic/FinalProject/src/main/java/business/service/AccountService.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import persistence.dao.AccountDAO;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 @Service
 public class AccountService {
@@ -17,19 +21,22 @@ public class AccountService {
         return accountDAO.deleteAccount(userName);
     }
 
-    public int updateUserLogIn(boolean loggedIn,String userName){
-        return accountDAO.updateUserLogIn(loggedIn,userName);
+    public int updateUserLogIn(boolean loggedIn, String userName) {
+        return accountDAO.updateUserLogIn(loggedIn, userName);
     }
 
 
     public String cryptPass(String password) {
-        char[] chars = password.toCharArray();
-        StringBuilder newPassword = new StringBuilder(" ");
-        for (char c : chars) {
-            c += 3;
-            newPassword.append(c);
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
-        return newPassword.toString();
+        BigInteger bigInteger = new BigInteger(1, messageDigest.digest(password.getBytes()));
+        String cryptedPassword = bigInteger.toString();
+
+        return cryptedPassword;
     }
 
     public String checkRegistration(String userName, String password) {
